b72eb610837e26aa6d18c5817453574c9f2fef56
HUSEYIN OZDEMIR <huseyinozdemir1419@gmail.com>
2021-05-25 12:36:18 +0300

    update

--- salat-vakt.sh
+++ salat-vakt.sh
@@ -29,11 +29,11 @@ i=1; while [ $(($i)) -le 18 ]; do
   # it should not show second line, so start counting from 1
   i=$(($i + 1))
   
-  # Günün hangi vaktini istiyorsun onu değişkene ata. i=1 iken imsak, i=7 iken yatsı gibi
+  # günün hangi vaktini istiyorsun onu değişkene ata. i=1 iken imsak, i=7 iken yatsı gibi
   # 
   vakt=`grep -i "gece" $file | cut -d, -f$i`
   
-  # Bugünün tarihi ile arama yaparak istediğim vakitin zamanını öğren
+  # bugünün tarihi ile arama yaparak istediğim vakitin zamanını öğren
   # search with todays date
   time=`grep "$date" $file | cut -d, -f$i`
   
